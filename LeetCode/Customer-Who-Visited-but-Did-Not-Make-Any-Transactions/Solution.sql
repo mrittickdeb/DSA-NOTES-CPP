22c22925362e8f49b8102b763f9faf911e0a364d
Mrittick Deb <147146704+lordinsane07@users.noreply.github.com>
2026-05-03 16:45:13 +0530

    Add solution for Customer Who Visited but Did Not Make Any Transactions

---
+++ LeetCode/Customer-Who-Visited-but-Did-Not-Make-Any-Transactions/Solution.sql
@@ -0,0 +1,3 @@
+1# Write your MySQL query statement below
+2SELECT v.customer_id,COUNT(v.customer_id) AS count_no_trans FROM 
+3Visits v LEFT JOIN Transactions t ON v.visit_id=t.visit_id WHERE t.visit_id IS NULL GROUP BY v.customer_id ORDER BY count_no_trans DESC;
