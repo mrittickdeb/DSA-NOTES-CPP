76cbe3b605cb257abb8b3d3e304e40aa5251bf0c
Mrittick Deb <147146704+lordinsane07@users.noreply.github.com>
2026-05-11 22:50:36 +0530

    Time: 111 ms (33.57%) | Memory: 95.2 MB (92.67%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -2,23 +2,53 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int n=nums.size();
-        int l=0;
-       unordered_map<long,long> mpp;
-       long long maxsum=0,sum=0;
-       for(int r=0;r<n;r++){
-        mpp[nums[r]]++;
-        sum+=nums[r];
-        if(r-l+1>k){
-            mpp[nums[l]]--;
-            if(mpp[nums[l]]==0) mpp.erase(nums[l]);
-            sum-=nums[l];
-            l++;
+        int i=0;
+        unordered_map<int,int> mpp;
+       long long sum=0;
+        long long maxi=0;
+        for(int j=0;j<n;j++){
+            sum+=nums[j];
+            mpp[nums[j]]++;
+            if(j-i+1>k){
+                sum-=nums[i];
+                mpp[nums[i]]--;
+                if(mpp[nums[i]]==0) mpp.erase(nums[i]); 
+                i++;
+            }
+        if(j-i+1==k && mpp.size()==k) {
+            maxi=max(maxi,sum);
         }
-        if(r-l+1==k && mpp.size()==k)
-        {
-            maxsum=max(maxsum,sum);
         }
-       }
-       return maxsum;
+    
+    return maxi;
+    
     }
+
+
+
+
+
+
+
+
+    //     int n=nums.size();
+    //     int l=0;
+    //    unordered_map<long,long> mpp;
+    //    long long maxsum=0,sum=0;
+    //    for(int r=0;r<n;r++){
+    //     mpp[nums[r]]++;
+    //     sum+=nums[r];
+    //     if(r-l+1>k){
+    //         mpp[nums[l]]--;
+    //         if(mpp[nums[l]]==0) mpp.erase(nums[l]);
+    //         sum-=nums[l];
+    //         l++;
+    //     }
+    //     if(r-l+1==k && mpp.size()==k)
+    //     {
+    //         maxsum=max(maxsum,sum);
+    //     }
+    //    }
+    //    return maxsum;
+    // }
 };
