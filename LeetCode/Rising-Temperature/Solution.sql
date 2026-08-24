d442f16ffabff456f37866b7b710c9c703f593d4
Mrittick Deb <147146704+mrittickdeb@users.noreply.github.com>
2026-08-25 00:13:10 +0530

    Add solution for Rising Temperature

--- LeetCode/Rising-Temperature/Solution.sql
+++ LeetCode/Rising-Temperature/Solution.sql
@@ -1,3 +1,2 @@
-SELECT w1.id
-FROM Weather w1, Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+1# Write your MySQL query statement below
+2select w1.id from Weather w1,Weather w2 where DATEDIFF(w1.recordDate,w2.recordDate)=1 AND w1.temperature>w2.temperature; 
